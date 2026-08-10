c3a7b3d784ba485b1ec1acf0cdccc2c300977e57
Jordan Sede <sedejordan88@gmail.com>
2026-08-10 11:09:30 +0100

    database.py changes

--- database.py
+++ database.py
@@ -604,10 +604,10 @@ def init_db():
                 );
             """)
 
-            cursor.execute("""
-                INSERT INTO admin_users (user_id) VALUES (1);
-            """)
-            print("✅ Added someone to admin_users")
+            # cursor.execute("""
+            #     INSERT INTO admin_users (user_id) VALUES (1);
+            # """)
+            # print("✅ Added someone to admin_users")
             
             print("✅ Created/verified flagged_users table")
 
